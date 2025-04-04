e06dc1a6fabccc4a113bdf2731dd3e98b216cef4
developer111222 <anishprajapati765@gmail.com>
2025-04-04 12:40:03 +0530

    first commit

--- src/utils/Loader.js
+++ src/utils/Loader.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Loader = () => {
   return (
-<div className="h-screen flex items-center justify-center bg-gray-100">
+<div className="h-screen flex items-center justify-center ">
       {/* Spinner */}
-      <div className="w-16 h-16 border-4 border-t-4 border-blue-500 border-solid rounded-full animate-spin"></div>
+      <div className="w-16 h-16 border-4 border-t-4 border-white-500 border-solid rounded-full animate-spin"></div>
     </div>
   )
 }
