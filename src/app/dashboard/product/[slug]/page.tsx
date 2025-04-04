3a15f74fced283511116fa705c97b64fa3eb94b7
developer111222 <anishprajapati765@gmail.com>
2025-04-04 16:57:51 +0530

    first commit

--- src/app/dashboard/product/[slug]/page.tsx
+++ src/app/dashboard/product/[slug]/page.tsx
@@ -25,10 +25,9 @@ const Page = () => {
     title: "",
     content: "",
     image: null as File | null,
-    preview: null as string | null,
-    id: ''
+    preview: null as string | null, // This will hold the preview image URL
+    id:''
   });
-  
 
   useEffect(() => {
     if (slug) {
@@ -51,13 +50,12 @@ router.push('/dashboard/allproducts')
       setFormData({
         title: singleProduct.title,
         content: singleProduct.content,
-        image: null, // Keep as null or file type depending on your requirements
-        preview: singleProduct.image || null, // Correct key here
-        id: singleProduct._id // Assuming product ID is part of the response
+        image: null, // We'll use the image preview
+        preview: singleProduct.image || null, // Assuming product image is a URL
+        id: singleProduct._id, // Assuming product ID is in the response
       });
     }
   }, [singleProduct]);
-  
 
   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
